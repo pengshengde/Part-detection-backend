339411465f54b9bf04a68a8063ed537caa526f87
彭圣德 <35351057250@qq.com>
2023-11-05 06:16:26 +0000

    更新 run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 ps -ef | grep java | awk '{print $2}' | xargs kill -9
 
 # start python flask
-nohup java -jar springboot1-0.0.1-SNAPSHOT.jar > /home/peng/springboot1.log 2>&1 &
+nohup java -jar springboot1-0.0.1-SNAPSHOT.jar > /home/gitlab-runner/springboot1.log 2>&1 &
