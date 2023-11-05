752bf3c011e1dac850878553a98c777b705baa16
彭圣德 <35351057250@qq.com>
2023-11-05 06:17:47 +0000

    更新 run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # check and kill xistance of current flask process
-ps -ef | grep java | awk '{print $2}' | xargs kill -9
+# ps -ef | grep java | awk '{print $2}' | xargs kill -9
 
 # start python flask
 nohup java -jar springboot1-0.0.1-SNAPSHOT.jar > /home/gitlab-runner/springboot1.log 2>&1 &
